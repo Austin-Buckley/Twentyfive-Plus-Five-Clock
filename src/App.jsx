c2960ce1c0ee337d82e089c31f8db9da67fcf993
Austin Buckley <austinbuckley2023@gmail.com>
2024-06-07 02:33:16 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div className="hello">Hello World</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
